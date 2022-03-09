ecdaf7ce3e22e345211fc412efa2a37728e01367
Abdoulaye-sys <81588052+Abdoulaye-sys@users.noreply.github.com>
2022-03-09 21:16:05 +0100

    suite

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/abdoulaye_portfolio-ConvertImage.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers}  from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Apprendre à me connaître</h5>
+      <h2>À propos de moi</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='about me' />
+          </div>
+        </div>
+
+
+        <div className='about__content'>
+          <div className='about__cards'>
+
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small> Stage développeur mobile</small>
+            </article>
+
+              <article className='about__card'>
+                <FiUsers className='about__icon'/>
+                <h5>Clients</h5>
+                <small>200+ worldwide</small>
+              </article>
+
+
+              <article className='about__card'>
+                <VscFolderLibrary className='about__icon'/>
+                <h5>Projet</h5>
+                <small> 3+ projets complétés</small>
+              </article>
+
+          </div>
+
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,  Ipsum.
+            </p>
+
+            <a href='#contact' className='btn btn-primary'>Parlons</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>        
+        <a href='https://www.linkedin.com/in/abdoulaye-ba98/' target="_blank"><BsLinkedin/></a>        
         <a href='https://github.com' target="_blank"><FaGithub/></a>
     </div>
   )
