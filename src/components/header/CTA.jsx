30e3ac4795addd40f124369bfde19fe7a0248394
Abdoulaye-sys <81588052+Abdoulaye-sys@users.noreply.github.com>
2023-09-01 16:34:55 +0200

    newCv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Cv_Abdoulaye_Ba.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Télécharger mon CV</a>
+        <a href={CV} download className='btn'>Ici Vous pouvez Télécharger mon CV</a>
         <a href='#contact' className='btn btn-primary'>Me Contacter</a>
     </div>
   )
