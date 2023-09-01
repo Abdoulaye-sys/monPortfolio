bb8d00a02bc98a10d7c765543d7801b0229b74e4
Abdoulaye-sys <81588052+Abdoulaye-sys@users.noreply.github.com>
2023-09-01 17:22:04 +0200

    Votre message de commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/Cv_Abdoulaye_Ba.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Ici Vous pouvez Télécharger mon CV</a>
-        <a href='#contact' className='btn btn-primary'>Me Contacter</a>
+        <a href={CV} download className='btn'>Télécharger mon CV</a>
+        <a href='#contact' className='btn btn-primary'>On échange ? </a>
     </div>
   )
 }
